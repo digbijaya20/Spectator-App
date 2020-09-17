00a1074e865edb7c4a70c03ab1e2ac68b02ea8f5
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2020-09-17 16:16:19 +0530

    change

---
+++ client/build/precache-manifest.33d6a229ccf99359634bf118329a06f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4ad3e1e0dee0de6eed2583308ad3d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2abefedfce3f1d3fbf36",
+    "url": "/static/css/main.330e4d25.chunk.css"
+  },
+  {
+    "revision": "30907c58d5487f02be27",
+    "url": "/static/js/2.ef56cb3c.chunk.js"
+  },
+  {
+    "revision": "2abefedfce3f1d3fbf36",
+    "url": "/static/js/main.77c47da8.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
